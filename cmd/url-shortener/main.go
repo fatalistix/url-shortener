84f8fc89566a8ae8b4e2679bf77caf0df7049d80
fatalistix <slavabal11@outlook.com>
2024-04-11 23:58:36 +0700

    created application startup

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	"context"
+	"github.com/fatalistix/url-shortener/internal/http-server/handlers/url/del"
+	"github.com/fatalistix/url-shortener/internal/http-server/handlers/url/save"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/fatalistix/url-shortener/internal/config"
+	"github.com/fatalistix/url-shortener/internal/env"
+	"github.com/fatalistix/url-shortener/internal/http-server/handlers/redirect"
+	mwLogger "github.com/fatalistix/url-shortener/internal/http-server/middleware/logger"
+	"github.com/fatalistix/url-shortener/internal/lib/logger/sl"
+	"github.com/fatalistix/url-shortener/internal/storage/sqlite"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// init env
+	env.MustLoad()
+
+	// init config: cleanenv
+	// путь к конфигу задается через переменную окружения (можно и иначе, но как-будто это выглядит в стиле го)
+	configPath := os.Getenv("CONFIG_PATH")
+	cfg := config.MustLoad(configPath)
+
+	// init logger: slog
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
+
+	// init storage: sqlite3
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+	defer func() {
+		if err := storage.Close(); err != nil {
+			log.Error("failed to close storage", sl.Err(err))
+		} else {
+			log.Info("storage closed")
+		}
+	}()
+
+	// init router: chi
+	router := chi.NewRouter()
+
+	// middleware
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	//router.Post("/url", save.New(log, storage))
+
+	router.Route("/url", func(r chi.Router) {
+		r.Use(middleware.BasicAuth("url-shortener", map[string]string{
+			cfg.HTTPServer.User: cfg.HTTPServer.Password,
+		}))
+
+		r.Post("/", save.New(cfg.Service.AliasLength, log, storage))
+		r.Delete("/{alias}", del.New(log, storage))
+	})
+
+	router.Get("/{alias}", redirect.New(log, storage))
+
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	// graceful shutdown by catching os signals
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+
+	// run server
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.Timeout,
+		WriteTimeout: cfg.Timeout,
+		IdleTimeout:  cfg.IdleTimeout,
+	}
+
+	go func() {
+		if err = srv.ListenAndServe(); err != nil {
+			log.Error("server stopped with error", sl.Err(err))
+		}
+	}()
+
+	log.Info("server started")
+
+	<-done
+	log.Info("stopping server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.GracefulShutdownTimeout)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Error("failed to stop server", sl.Err(err))
+
+		return
+	}
+
+	log.Info("server stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	default:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
