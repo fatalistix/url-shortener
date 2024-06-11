77f432500e592b40728d13021fb940396e6617fa
fatalistix <slavabal11@outlook.com>
2024-06-12 02:49:41 +0700

    changed to must load

--- internal/env/env.go
+++ internal/env/env.go
@@ -1,14 +1,12 @@
 package env
 
 import (
-	"log"
-
 	"github.com/joho/godotenv"
 )
 
 func MustLoad() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal(".env file does not exists")
+		panic(".env file does not exists")
 	}
 }
